5162b4e23edde5ccd77ae027eaf0c3ac15c62070
Florian REY <39830513+OrangePeelFX@users.noreply.github.com>
2018-08-02 14:21:45 +0200

    Add methods

--- Scripts/002_Loading_An_Image/Sources/Project.hx
+++ Scripts/002_Loading_An_Image/Sources/Project.hx
@@ -6,10 +6,10 @@ import kha.Assets;
 import kha.Image;
 
 class Project {
-	public var image:Image;
+	public var image:Image; 
 
 	public function new(){
-		image = Assets.images.Haxe_Logo;
+		image = Assets.images.Haxe_Logo; //original image 230 x 314
 	}
 
 	public function update():Void {
@@ -18,7 +18,9 @@ class Project {
 	public function render(framebuffer:Framebuffer):Void {
 		var g = framebuffer.g2;
 		g.begin();
-		g.drawImage(image, 30, 30);
+		g.drawImage(image, 0, 0);
+		g.drawScaledImage(image, 240, 0, image.width/2, image.height/2); 
+		g.drawScaledSubImage(image, 0, 0, image.width/2, image.height/2, 240, 157, image.width/2, image.height/2);
 		g.end();
 	}
 
