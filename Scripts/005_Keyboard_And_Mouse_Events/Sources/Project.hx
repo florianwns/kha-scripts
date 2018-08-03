8f7816ccc451667ca33f133be433be5657f2eb45
Florian REY <39830513+OrangePeelFX@users.noreply.github.com>
2018-08-03 08:36:13 +0200

    Update Keyboard

--- Scripts/005_Keyboard_And_Mouse_Events/Sources/Project.hx
+++ Scripts/005_Keyboard_And_Mouse_Events/Sources/Project.hx
@@ -1,20 +1,50 @@
 package;
 
 import kha.Framebuffer;
+import kha.Assets;
 import kha.input.Mouse;
 import kha.input.Keyboard;
 import kha.input.KeyCode;
+import kha.graphics2.Graphics;
+import kha.Color;
 
 class Project {
+	public var arrows:Array<Bool>;
+	public var text:String;
+
 	public function new(){
 			Mouse.get().notify(onMouseDown, onMouseUp, onMouseMove, onMouseWheel, onMouseLeave);
 			Keyboard.get().notify(onKeyDown, onKeyUp, onKeyPress);
+			arrows = [false,false,false,false]; // up, right, down, left
+			text = "";
 	}
 
 	public function update():Void {
 	}
 
 	public function render(framebuffer:Framebuffer):Void {
+		drawGraphics(framebuffer.g2);
+	}
+
+	private function drawGraphics(g:Graphics){
+		g.begin();
+		{
+			g.color = Color.White;
+			g.font = Assets.fonts.OpenSans;
+			g.fontSize = 25;
+			var w = kha.System.windowWidth();
+			var h = kha.System.windowHeight();
+
+			if(arrows[0] == true) g.drawString("UP", w / 2, h / 4);
+			if(arrows[1] == true) g.drawString("RIGHT", w * 3/4, h / 2);
+			if(arrows[2] == true) g.drawString("DOWN", w / 2, h * 3/4);
+			if(arrows[3] == true) g.drawString("LEFT", w / 4, h / 2);
+			if(text != ""){
+				g.drawString(text, 10, 10);
+				//this.text = "";
+			}
+		}
+		g.end();
 	}
 
 	public function onMouseDown(button:Int, x:Int, y:Int):Void {
@@ -37,14 +67,26 @@ class Project {
 	}
 
 	public function onKeyDown(keyCode:KeyCode):Void {
-		trace("Key Down : " + keyCode);
+		switch(keyCode){
+			case KeyCode.Up 		: arrows[0] = true;
+			case KeyCode.Right 	: arrows[1] = true;
+			case KeyCode.Down  	: arrows[2] = true;
+			case KeyCode.Left  	: arrows[3] = true;
+			default: return; // Important 
+		}
 	}
 
 	public function onKeyUp(keyCode:KeyCode):Void {
-		trace("Key Up : " + keyCode);
+		switch(keyCode){
+			case KeyCode.Up 		: arrows[0] = false;
+			case KeyCode.Right 	: arrows[1] = false;
+			case KeyCode.Down  	: arrows[2] = false;
+			case KeyCode.Left  	: arrows[3] = false;			
+			default: return; // Important 
+		}
 	}
 	
 	public function onKeyPress(key:String):Void {
-		trace("Key Press : " + key);
+		this.text += key;
 	}
 }
