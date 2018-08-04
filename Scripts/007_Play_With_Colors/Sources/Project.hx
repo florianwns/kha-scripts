129ce674c6e0655fead9d487788ddc6d9070ea3a
REY Florian <florian@MacBook-Pro-de-Florian.local>
2018-08-04 09:48:36 +0200

    Indent

--- Scripts/007_Play_With_Colors/Sources/Project.hx
+++ Scripts/007_Play_With_Colors/Sources/Project.hx
@@ -8,12 +8,12 @@ class Project {
 
 	public function new(){
 		colors = [
-			Color.Green, 														// Initialisation avec une couleur prédéfinie
-			Color.fromBytes(125,0,124,255),					// Initialisation avec des entiers de 0 à 255 (RGBA)
+			Color.Green, 								// Initialisation avec une couleur prédéfinie
+			Color.fromBytes(125,0,124,255),				// Initialisation avec des entiers de 0 à 255 (RGBA)
 			Color.fromFloats(0.3,0.1,0.2,0.8),			// Initialisation avec des nombre à virgule 0 à 1 (RGBA)
-			Color.fromValue(0xffffff00),						// Initialisation avec un entier codé sur 32 bits (ARGB)
-			Color.fromString("#ffffff00"),					// Initialisation avec un entier codé 32 bits (ARGB)
-			Color.fromString("#ff00ff")							// Initialisation avec un entier codé 24 bits (RGB)
+			Color.fromValue(0xffffff00),				// Initialisation avec un entier codé sur 32 bits (ARGB)
+			Color.fromString("#ffffff00"),				// Initialisation avec un entier codé 32 bits (ARGB)
+			Color.fromString("#ff00ff")					// Initialisation avec un entier codé 24 bits (RGB)
 		];
 	}
 
